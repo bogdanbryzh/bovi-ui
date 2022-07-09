4c9031de814fa48157f5b06bd22eabe3929cbae1
Bogdan B Peterson <bogdan.bryzh@gmail.com>
2022-07-09 15:39:56 +0300

    button test

---
+++ packages/button/src/lib/button.spec.ts
@@ -0,0 +1,7 @@
+import { button } from './button';
+
+describe('button', () => {
+  it('should work', () => {
+    expect(button()).toEqual('button');
+  });
+});
---
+++ packages/button/src/lib/button.ts
@@ -0,0 +1,3 @@
+export function button(): string {
+  return 'button';
+}
